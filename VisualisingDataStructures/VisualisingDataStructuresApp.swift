2e02328c594cb2577d1d459154cbbbce87d47ca5
Diab1716 <diab.ahmed@etoncollege.org.uk>
2022-03-19 10:42:11 +0000

    Initial Commit

---
+++ VisualisingDataStructures/VisualisingDataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataStructuresApp.swift
+//  VisualisingDataStructures
+//
+//  Created by Diab, Ahmed (HWTA) on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
